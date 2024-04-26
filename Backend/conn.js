27dca09cd887886edb363be2a3b70d5492e0313a
Maria <msola@uninorte.edu.co>
2024-04-26 02:03:15 -0500

    agregacion query 1, incorporacion de queries-funciones individuales funcionales

--- Backend/conn.js
+++ Backend/conn.js
@@ -100,48 +100,167 @@ var config = {
 //------------------------------------------------------------------
 
 
-//QUERIE 2
-function get_crimedata_from_quantity(quantity, callback) {
-    // connect to your database
-    const pool = new sql.ConnectionPool(config);
+// //QUERIE 2
+// function get_crimedata_from_quantity(quantity, callback) {
+//     // connect to your database
+//     const pool = new sql.ConnectionPool(config);
+//     pool.connect().then(pool => {
+//         // create Request object
+//         const request = new sql.Request(pool);
+
+//         const query = `
+//             SELECT Vict_Sex,
+//                 DATE_OCC AS Fecha,
+//                 COUNT(*) AS Cantidad,
+//                 (COUNT(*) * 100.0 / 
+//                     (SELECT COUNT(*) 
+//                     FROM Crime_Data 
+//                     WHERE Vict_Sex IN ('F', 'M', 'X')
+//                     )
+//                 ) AS Porcentaje
+//             FROM Crime_Data
+//             WHERE Vict_Sex IN ('F', 'M', 'X')
+//             GROUP BY Vict_Sex, DATE_OCC
+//             ORDER BY Cantidad DESC;
+//         `;
+
+//         // query to the database and get the records
+//         request.query(query, function (err, recordset) {
+//             if (err) {
+//                 console.log(err);
+//                 return callback(err);
+//             }
+
+//             // send records as a response
+//             callback(null, recordset.recordset);
+
+//             // close the pool
+//             pool.close();
+//         });
+//     }).catch(err => {
+//         console.log(err);
+//         return callback(err);
+//     });
+// }
+// module.exports = {get_crimedata_from_quantity}
+
+
+
+// Funciones para ejecutar consultas
+function get_crime_location_by_type(crm_cd_desc, callback) {
+    const queryString = `
+        SELECT LON, LAT
+        FROM Crime_Data
+        WHERE crm_cd_desc = @crm_cd_desc
+          AND LAT IS NOT NULL
+          AND LON IS NOT NULL;
+    `;
+
+    executeQuery(queryString, { crm_cd_desc }, callback);
+}
+//ejemplo de puerto: http://localhost:5000/get_crime_location/THEFT,%20PERSON
+
+//--------------------------------------------
+function get_victims_by_sex(date, callback) {
+    const queryString = `
+    SELECT Vict_Sex, COUNT(*) AS Cantidad
+    FROM Crime_Data
+    WHERE DATE_OCC = CONVERT(datetime, @date, 120) -- 120 es el formato para 'YYYY-MM-DD HH:MI:SS'
+      AND Vict_Sex IN ('F', 'M', 'X')
+    GROUP BY Vict_Sex;
+    
+    `;
+
+    executeQuery(queryString, { date }, callback);
+}
+// //ejemplo de puerto: http://localhost:5000/get_victims_by_sex/2021-04-25
+
+function get_crime_count_and_hours_by_type(crm_cd_desc, callback) {
+    const queryString = `
+        SELECT Crm_Cd_Desc, COUNT(Crm_Cd_Desc) AS NumeroDeCrimenes, TIME_OCC
+        FROM Crime_Data
+        WHERE Crm_Cd_Desc = @crm_cd_desc
+        GROUP BY Crm_Cd_Desc, TIME_OCC
+        ORDER BY TIME_OCC ASC;
+    `;
+    executeQuery(queryString, { crm_cd_desc }, callback);
+}
+//// EJEMPLO DE CONEXION: http://localhost:5000/get_crime_count_and_hours/VEHICLE%20-%20STOLEN
+
+function get_area_crime_concentration(areaName, callback) {
+    const queryString = `
+        SELECT AREA_NAME, LAT, LON, COUNT(*) AS Cantidad
+        FROM Crime_Data
+        WHERE AREA_NAME = @areaName
+        GROUP BY AREA_NAME, LAT, LON;
+    `;
+
+    executeQuery(queryString, { areaName }, callback);
+}
+
+////// EJEMPLO DE CONEXION: http://localhost:5000/get_area_crime_concentration/Newton
+
+function get_crime_count_by_type_and_hour(crm_cd_desc, callback) {
+    const queryString = `
+        SELECT TIME_OCC, COUNT(*) AS NumeroDeCrimenes
+        FROM Crime_Data
+        WHERE Crm_Cd_Desc = @crm_cd_desc
+        GROUP BY TIME_OCC;
+    `;
+
+    executeQuery(queryString, { crm_cd_desc }, callback);
+}
+/////////////////EJEMPLO DE CONEXION: http://localhost:5000/get_crime_count_by_type_and_hour/VEHICLE%20-%20STOLEN
+
+function get_gender_distribution_by_weapon(weapon_desc, callback) {
+    const queryString = `
+        SELECT Weapon_Desc, Vict_Sex, COUNT(*) AS Cantidad
+        FROM Crime_Data
+        WHERE Weapon_Desc = @weapon_desc
+          AND Vict_Sex IS NOT NULL
+          AND Vict_Sex != 'X'
+          AND Vict_Sex != 'H'
+        GROUP BY Weapon_Desc, Vict_Sex;
+    `;
+
+    executeQuery(queryString, { weapon_desc }, callback);
+}
+
+///////////////////EJEMPLO DE CONEXION: http://localhost:5000/get_gender_distribution_by_weapon/SHOTGUN
+
+function executeQuery(queryString, params, callback) {
+    const pool = new sql.ConnectionPool(config); // Crea una nueva  conexion
     pool.connect().then(pool => {
-        // create Request object
-        const request = new sql.Request(pool);
+        const request = new sql.Request(pool); // Crea una nueva solicitud
 
-        const query = `
-            SELECT Vict_Sex,
-                DATE_OCC AS Fecha,
-                COUNT(*) AS Cantidad,
-                (COUNT(*) * 100.0 / 
-                    (SELECT COUNT(*) 
-                    FROM Crime_Data 
-                    WHERE Vict_Sex IN ('F', 'M', 'X')
-                    )
-                ) AS Porcentaje
-            FROM Crime_Data
-            WHERE Vict_Sex IN ('F', 'M', 'X')
-            GROUP BY Vict_Sex, DATE_OCC
-            ORDER BY Cantidad DESC;
-        `;
+        // Agrega parámetros a la solicitud
+        for (let key in params) {
+            request.input(key, params[key]);
+        }
 
-        // query to the database and get the records
-        request.query(query, function (err, recordset) {
+        // Ejecuta la consulta
+        request.query(queryString, function (err, recordset) {
             if (err) {
                 console.log(err);
+                pool.close(); 
                 return callback(err);
             }
 
-            // send records as a response
             callback(null, recordset.recordset);
-
-            // close the pool
-            pool.close();
+            pool.close(); 
         });
     }).catch(err => {
         console.log(err);
         return callback(err);
     });
 }
-module.exports = {get_crimedata_from_quantity}
 
+module.exports = {
+    get_crime_location_by_type,
+    get_victims_by_sex,
+    get_crime_count_and_hours_by_type,
+    get_area_crime_concentration,
+    get_crime_count_by_type_and_hour,
+    get_gender_distribution_by_weapon,
 
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,8 @@
-const con = require("./conn")
-
 const express = require('express');
 const app = express();
+const con = require("./conn")
 
-
+//--------------------------------------------------------------
 // app.get('/get_crimes_data/:quantity', function (req, res) {
     
 //     var quantity = req.params.quantity
@@ -11,17 +10,96 @@ const app = express();
 //     res.send(crimedata)
 // });
 
-app.get('/get_crimes_data/:quantity', function (req, res) {
-    var quantity = req.params.quantity;
-    con.get_crimedata_from_quantity(quantity, function (err, crimedata) {
+// app.get('/get_crimes_data/:quantity', function (req, res) {
+//     var quantity = req.params.quantity;
+//     con.get_crimedata_from_quantity(quantity, function (err, crimedata) {
+//         if (err) {
+//             return res.status(500).send(err);
+//         }
+//         res.send(crimedata);
+//     });
+// });
+
+
+// var server = app.listen(5000, function () {
+//     console.log('Server is running..');
+// });
+//---------------------------------------------------------------
+
+// Ruta para obtener la ubicación de crímenes por tipo de delito
+app.get('/get_crime_location/:crime_type', function (req, res) {
+    const crime_type = req.params.crime_type;
+    con.get_crime_location_by_type(crime_type, function (err, result) {
         if (err) {
             return res.status(500).send(err);
         }
-        res.send(crimedata);
+        res.send(result);
+    });
+});
+
+// // Ruta para obtener la cantidad de víctimas por sexo en una fecha específica
+app.get('/get_victims_by_sex/:date', function (req, res) {
+    const date = req.params.date;
+    con.get_victims_by_sex(date, function (err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
     });
 });
 
 
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
+// Ruta para obtener la cantidad de crímenes y las horas en que ocurrieron por tipo de delito
+app.get('/get_crime_count_and_hours/:crime_type', function (req, res) {
+    let crime_type = req.params.crime_type;
+    crime_type = decodeURIComponent(crime_type);
+
+    con.get_crime_count_and_hours_by_type(crime_type, function (err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+
+
+// // Ruta para obtener la concentración de crímenes por área
+app.get('/get_area_crime_concentration/:areaName', function (req, res) {
+    const areaName = req.params.areaName; 
+
+    con.get_area_crime_concentration(areaName, function (err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+
+// // Ruta para obtener la cantidad de crímenes por tipo y hora
+app.get('/get_crime_count_by_type_and_hour/:crime_type', function (req, res) {
+    const crime_type = req.params.crime_type;
+    con.get_crime_count_by_type_and_hour(crime_type, function (err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+// // Ruta para obtener la distribución de género por arma
+app.get('/get_gender_distribution_by_weapon/:weapon_desc', function (req, res) {
+    const weapon_desc = req.params.weapon_desc;
+    con.get_gender_distribution_by_weapon(weapon_desc, function (err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+// Iniciar el servidor
+const server = app.listen(5000, function () {
+    console.log('Server is running on port 5000');
 });
