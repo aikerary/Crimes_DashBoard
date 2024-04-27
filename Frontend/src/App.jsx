4cf35087e564814f1adf22058a2c424065449333
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-04-26 22:43:19 -0500

    installing leaflite

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
