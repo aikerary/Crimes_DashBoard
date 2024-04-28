ad04e3749b00c4ad55fa813b6505b87b06a3b6f1
t-santoya <139002439+t-santoya@users.noreply.github.com>
2024-04-28 12:33:55 -0500

    actualizacion en el prop y creacion del header

--- Frontend/src/components/Prop/Prop.jsx
+++ Frontend/src/components/Prop/Prop.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './Prop.css'; // Importar los estilos CSS
 
 const CrimeDashboard = () => {
   return (
-    <div style={{ color: 'white', textAlign: 'center', marginRight: '320px' }}>
-      <h1>Crime Dashboard 2020 to March 2024</h1>
-      <p style={{ margin: '0 auto', maxWidth: '600px' }}>The purpose of this Dashboard is to provide a visual tool that allows users to explore and understand crime patterns and trends in the city.</p>
+    <div className="crime-dashboard-container">
+      <p>The purpose of this Dashboard is to provide a visual tool that allows users to explore and understand crime patterns and trends in the city.</p>
     </div>
   );
 }
