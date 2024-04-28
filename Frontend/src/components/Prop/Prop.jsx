7a77baf219d5a406efd33c8540f4a22ca5121eed
HamedPro3D <elneserh@uninorte.edu.co>
2024-04-27 23:59:29 -0500

    Alineando texto al medio.

--- Frontend/src/components/Prop/Prop.jsx
+++ Frontend/src/components/Prop/Prop.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CrimeDashboard = () => {
   return (
-    <div style={{ color: 'white' }}>
+    <div style={{ color: 'white', textAlign: 'center', marginRight: '320px' }}>
       <h1>Crime Dashboard 2020 to March 2024</h1>
-      <p>The purpose of this Dashboard is to provide a visual tool that allows users to explore and understand crime patterns and trends in the city.</p>
+      <p style={{ margin: '0 auto', maxWidth: '600px' }}>The purpose of this Dashboard is to provide a visual tool that allows users to explore and understand crime patterns and trends in the city.</p>
     </div>
   );
 }
