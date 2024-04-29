3fe683c98fd343695df484954d9e2cc14ae8e0bb
ahiruk <luisaguzmanidetp@gmail.com>
2024-04-28 21:36:55 -0500

    SOY LA MAS CAPITA

--- Frontend/src/components/AreaTotal/Area_Total.jsx
+++ Frontend/src/components/AreaTotal/Area_Total.jsx
@@ -37,30 +37,41 @@ const AreaCountChart = () => {
   });
 
   useEffect(() => {
-    fetch('http://localhost:5000/get_area_count_total')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const areaNames = data.map(item => item.AREA_NAME);
-        const counts = data.map(item => item.NumeroDeRegistros);
+    const fetchData = () => {
+      fetch('http://localhost:5000/get_area_count_total')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          const areaNames = data.map(item => item.AREA_NAME);
+          const counts = data.map(item => item.NumeroDeRegistros);
 
-        setChartData(prevState => ({
-          ...prevState,
-          series: [{
-            data: areaNames.map((name, index) => ({
-              x: name,
-              y: counts[index]
-            }))
-          }]
-        }));
-      })
-      .catch(error => {
-        console.error('Error fetching area count data:', error);
-      });
+          setChartData(prevState => ({
+            ...prevState,
+            series: [{
+              data: areaNames.map((name, index) => ({
+                x: name,
+                y: counts[index]
+              }))
+            }]
+          }));
+        })
+        .catch(error => {
+          console.error('Error fetching area count data:', error);
+        });
+    };
+
+    // Fetch data initially
+    fetchData();
+
+    // Fetch data every 5 minutes (300,000 milliseconds)
+    const intervalId = setInterval(fetchData, 5000);
+
+    // Cleanup interval on unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
