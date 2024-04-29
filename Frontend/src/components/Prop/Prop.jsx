f8892507027ae285b526ac69238876ee34479f12
Maria <msola@uninorte.edu.co>
2024-04-28 21:37:48 -0500

    texto inicial

--- Frontend/src/components/Prop/Prop.jsx
+++ Frontend/src/components/Prop/Prop.jsx
@@ -3,8 +3,8 @@ import './Prop.css'; // Importar los estilos CSS
 
 const CrimeDashboard = () => {
   return (
-    <div className="crime-dashboard-container">
-      <p>The purpose of this Dashboard is to provide a visual tool that allows users to explore and understand crime patterns and trends in the city of Los Angeles.</p>
+    <div className="crime-dashboard-container" style={{ textAlign: 'center' }}>
+      <p>This dataset provides a detailed insight into reported crimes in Los Angeles from 2020 to the present. With the implementation of a new records management system by the Los Angeles Police Department (LAPD) in response to the FBI's mandate to exclusively collect data from the National Incident-Based Reporting System (NIBRS), improvements in data quality and accuracy are expected. However, during the transition to the new system, users may temporarily have access only to incidents reported in the previous system, while the LAPD works on generating new NIBRS datasets for a more efficient reporting system. It's important to note that due to the nature of original crime reports, which are often manually entered, there may be some inaccuracies in the data. Additionally, some location fields may contain incomplete or rounded data for privacy reasons. Despite these challenges, the primary goal is to provide an accurate representation of criminal activity in the city and address any questions or concerns that may arise in the comments.</p>
     </div>
   );
 }
